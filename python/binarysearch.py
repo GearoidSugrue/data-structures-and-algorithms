ab13a6ce1fc569e051532612b78bf9f7ac82f01f
Gearoid <gearoid.sugrue@outlook.com>
2016-07-12 14:55:11 +0100

    Enable binary_search to be run from command line

--- python/binarysearch.py
+++ python/binarysearch.py
@@ -2,6 +2,9 @@
 Efficiency is O(log(n))"""
 #coded with the help of Udacity :D
 
+import sys
+import ast
+
 def binary_search(input_array, value):
     """Returns the index of value, or -1 if the value
     doesn't exist in the list."""
@@ -15,7 +18,14 @@ def binary_search(input_array, value):
         if input_array[middle] < value:
             low = middle + 1
         elif input_array[middle] > value:
-            high = middle -1
+            high = middle - 1
         else:
             return middle
     return -1
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        #ast.literal_eval() only considers a small subset of syntax to be valid
+        inputList = ast.literal_eval(sys.argv[1])#parses input to type list
+        inputValue = ast.literal_eval(sys.argv[2])#parses input to int
+        print binary_search(inputList, inputValue)
